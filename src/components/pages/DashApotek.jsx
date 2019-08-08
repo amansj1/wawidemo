6cc4565245b731b94b40b7003e41a044d41ceb89
aman_sj <aman.gulo@si.ukdw.ac.id>
2019-08-08 17:03:27 +0700

    hahaha

--- src/components/pages/DashApotek.jsx
+++ src/components/pages/DashApotek.jsx
@@ -11,7 +11,7 @@ export default class DashApotek extends React.Component{
 				<img src="https://3a09223b3cd53870eeaa-7f75e5eb51943043279413a54aaa858a.ssl.cf3.rackcdn.com/7d5bb860ceb361002c4e049986d69532fa272c0f-1562933491-5d2878f3-960x640.jpg" alt="img13"/>
 				<figcaption>
 					<h2>Input <span>Obat</span></h2>
-					<p>Menambahkan data obat kedalam administrasi puskesmas.</p>
+					<p>Menambahkan data obat kedalam administrasi apotek.</p>
 					<Link to="/inputobat"></Link>
 				</figcaption>			
 			</figure>
