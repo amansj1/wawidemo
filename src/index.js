91f01587f435222fc441de3261e50a9a1342b1ed
aman_sj <aman.gulo@si.ukdw.ac.id>
2019-08-29 06:13:09 +0700

    ss

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
-import App from './App';
-// import Login from './Login';
+
+import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter  } from 'react-router-dom';
 
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><Login /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
