acac8f41db8259aff9ef8f9cee08ad5b9ea45322
aman_sj <aman.gulo@si.ukdw.ac.id>
2019-08-29 21:26:19 +0700

    dd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
-// import App from './App';
+
 import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter  } from 'react-router-dom';
