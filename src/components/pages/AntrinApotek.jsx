fbd84f1d6fca1655fd2fb4d0cd90c9f3e312ac6b
aman_sj <aman.gulo@si.ukdw.ac.id>
2019-08-30 16:36:35 +0700

    siapp bosquee

--- src/components/pages/AntrinApotek.jsx
+++ src/components/pages/AntrinApotek.jsx
@@ -10,29 +10,43 @@ export default class AntrinApotek extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            loading:false,
+            loading:true,
             modalup:false,
             rowKlik:false,
 
             colAntrian:[
             {title:'Id Antrian Apotek', field:'id_mst_antrian_apotek'},
-            {title:'Pasien', field:'mst_resep_obat[0].mst_pasien.name'},
-            {title:'Status', field:'status'}
+            {title:'Pasien', field:'mst_resep_obat.mst_pasien.name'},
+            {title:'Jenis Kelamin', field:'mst_resep_obat.mst_pasien.gender'}
             ],
             dataAntrian:[],
 
             colSelesai:[
             {title:'Id Antrian Apotek', field:'id_mst_antrian_apotek'},
-            {title:'Pasien', field:'mst_resep_obat[0].mst_pasien.name'},
-            {title:'Status', field:'status'}
+            {title:'Pasien', field:'mst_resep_obat.mst_pasien.name'},
+            {title:'Jenis Kelamin', field:'mst_resep_obat.mst_pasien.gender'}
             ],
             dataSelesai:[],
+
+            colreject:[
+              {title:'Id Antrian Apotek', field:'id_mst_antrian_apotek'},
+              {title:'Pasien', field:'mst_resep_obat.mst_pasien.name'},
+              {title:'Jenis Kelamin', field:'mst_resep_obat.mst_pasien.gender'}
+              ],
+              dataReject:[],
 
             idAntrianApotek:'',
             idApotek:1,
             idmstantrianpasien:'',
-            obat:[]
+            obat:[],
+            colObat:[
+              {title:'Nama Obat', field:'mst_nrm_opol.mst_obat.nama_obat'},
+              {title:'Takaran', field:'takaran_obat'},
+              {title:'Harga', field:'mst_nrm_opol.harga_obat'}
+            ],
+            namapasien:''
         }
+        this.toggle = this.toggle.bind(this);
     }
     toggle() {
       this.setState(prevState => ({
@@ -40,6 +54,7 @@ export default class AntrinApotek extends React.Component{
       }));
   };
     fetchdata = () =>{
+      Swal.showLoading();
 
         const url ='https://zav-wawi.herokuapp.com/api/antrian_apotek/hasprocessed/apotekid=';
         axios.get(url + this.props.id_pengguna)
@@ -50,7 +65,7 @@ export default class AntrinApotek extends React.Component{
             loading: false
           });
           // alert(response.data.note);
-          // console.log(response.data.data);
+          console.log(response.data.data);
         })
         .catch(error => {
         //  alert(error);
@@ -64,6 +79,21 @@ export default class AntrinApotek extends React.Component{
             dataSelesai: response.data.data,
             loading: false
           });
+          // alert(response.data.note);
+          console.log(response.data.data);
+        })
+        .catch(error => {
+          // alert(error.response);
+        });
+
+        const url2 ='https://zav-wawi.herokuapp.com/api/antrian_apotek/hasrejected/apotekid=';
+        axios.get(url2 + this.props.id_pengguna)
+        .then(response => {
+          this.setState({
+            dataReject: response.data.data,
+      
+          });
+          
           // alert(response.data.note);
           console.log(response.data.data);
         })
@@ -92,6 +122,7 @@ export default class AntrinApotek extends React.Component{
       .then(response => {
         // console.log(response);
         this.fetchdata();
+        this.toggle();
       })
       .catch(error => {
         console.log(error.response);
@@ -99,12 +130,13 @@ export default class AntrinApotek extends React.Component{
      }
      antrianhasreject = (e) =>{
       e.preventDefault();
-      const url4 ='https://zav-wawi.herokuapp.com/api/antrian_apotek/picked/id_mst_antrian_apotek=';
+      const url4 ='https://zav-wawi.herokuapp.com/api/antrian_apotek/rejected/id_mst_antrian_apotek=';
       console.log(url4+this.state.idmstantrianpasien);
       axios.get(url4+this.state.idmstantrianpasien)
       .then(response => {
         // console.log(response);
         this.fetchdata();
+        this.toggle();
       })
       .catch(error => {
         console.log(error.response);
@@ -115,8 +147,11 @@ export default class AntrinApotek extends React.Component{
       e.preventDefault();
       this.setState({
           idmstantrianpasien: rowData.id_mst_antrian_apotek,
-          // namaObat:rowData.nama_obat,
+          namapasien:rowData.mst_resep_obat.mst_pasien.name,
+          obat:rowData.mst_resep_obat.trans_obat,
       });
+     
+      this.toggle();
      
     }
     render(){
@@ -125,8 +160,9 @@ export default class AntrinApotek extends React.Component{
             content=Swal.showLoading();
         }else{
             content=
+            <div>
             <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-12">
                 <MTable 
                 title="Antrian Resep (Klik Baris Untuk Diproses)"
                 columns={this.state.colAntrian}
@@ -134,26 +170,43 @@ export default class AntrinApotek extends React.Component{
                 onRowClick={this.rowClik1}
                 />
             </div>
-            <div className="col-md-6">
-            <MTable 
+            </div>
+            <br/>
+            <div className="row">
+                 <div className="col-md-6">
+                  <MTable 
                 title="Antrian Resep (Sudah Diproses)"
                 columns={this.state.colSelesai}
                 data={this.state.dataSelesai}
                 />
+                </div>
+                <div className="col-md-6">
+                  <MTable 
+                title="Antrian Resep (Reject Resep)"
+                columns={this.state.colreject}
+                data={this.state.dataReject}
+                />
             </div>
-        </div>
+            </div>
+           </div>
+        
 
         }
         return(
         <div>{content}
          <Modal isOpen={this.state.modalup} toggle={this.toggle} className="modal-lg">
-          <ModalHeader toggle={this.toggle}><b>Resep Pasien</b></ModalHeader>
-         
+          <ModalHeader toggle={this.toggle}><b>Resep Pasien </b> {this.state.namapasien}</ModalHeader>
           <ModalBody>
-          
+          <MTable 
+                title="Daftar Obat"
+                columns={this.state.colObat}
+                data={this.state.obat}
+                />
           </ModalBody>
           <ModalFooter>
-            <Button type="submit" color="green">Submit</Button>
+          <Button type="button" color="red" onClick={this.antrianhasreject} >Reject Resep </Button>
+          <Button type="button" color="green" onClick={this. antrianhasproses} >Resep Selesai </Button>
+          
           </ModalFooter>
        
         </Modal>
